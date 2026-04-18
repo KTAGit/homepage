3bbdc3fe3f04e2163ce7a10e6b02b1eadb19be71
KTAGit <121195747+KTAGit@users.noreply.github.com>
2026-04-17 20:15:36 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+plugins: [new HtmlWebpackPlugin({ template: "./src/template.html" })];
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      { test: /\.html$/i, loader: "html-loader" },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  devtool: "eval-source-map",
+  devServer: { watchFiles: ["./src/template.html"] },
+};
